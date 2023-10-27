57b63d57748b874542ce1196bd41032e3c636c50
JesusGPro <145535842+JesusGPro@users.noreply.github.com>
2023-10-27 12:42:50 +0300

    Update ejemplo.py

--- .github/workflows/ejemplo.py
+++ .github/workflows/ejemplo.py
@@ -1,5 +1,11 @@
+import os
+
 def main():
   print("Hola desde github actions!!!")
+  token = os.environ.get("TOKEN_SECRETO")
+  if not token:
+    raise RuntimeError("TOKEN_SECRETO env var no esta puesto!!")
+  print("Todo guay! encontramos nuestra env var")
 
 if __name__ == '__main__':
         main()
